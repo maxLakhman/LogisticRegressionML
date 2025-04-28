a51187c2eff6761b4369f344529cedef1420d696
maxLakhman <msinghla@buffalo.edu>
2025-04-27 20:40:11 -0400

    Preprocessed the dataset into considering the first two features only

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,1 +1,7 @@
 from sklearn.datasets import load_iris
+
+
+# Load dataset and preprocess
+iris = load_iris()
+X = iris.data[:, 0:2]  # Select only two features for 2D visualization, must select better features X = iris.data[:, :2]
+print(X.shape)
