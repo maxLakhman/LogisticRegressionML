07f1080e1fd97f89bd5e1ff39c22e902c09abaad
maxLakhman <msinghla@buffalo.edu>
2025-05-11 15:06:28 -0400

    Found a resource on the iris dataset and how its represented

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,7 +1,13 @@
 from sklearn.datasets import load_iris
-
+#https://www.geeksforgeeks.org/iris-dataset/
 
 # Load dataset and preprocess
 iris = load_iris()
-X = iris.data[:, 0:2]  # Select only two features for 2D visualization, must select better features X = iris.data[:, :2]
-print(X.shape)
+X = iris.data[:, 0:2]  # Select only two features for 2D visualization, [:, 0:2] means all rows and first two columns
+print(X.shape)      #shape is given by (rows, columns)
+y = iris.target
+
+print(y.shape) #150 samples/rows. 1 column that represents the type of flower the features represent. The class it belongs to 
+
+feature_names = iris.feature_names[:2]
+print(feature_names)
